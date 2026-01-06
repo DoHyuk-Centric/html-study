2506e11c42714700f1ff1dcaae68ed4e497eda41
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-01-06 22:15:01 +0900

    26.1.6 캘린더

--- my_blog/components/devLog/calender.js
+++ my_blog/components/devLog/calender.js
@@ -1,22 +1,74 @@
-function createDay(){
-    const calender_day = document.getElementById('calender_day');
-    const newDay = document.createElement('div');
-    const newDayText = document.createTextNode('2');
+let date = new Date();
 
-    
+function renderCalendar() {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
 
-    newDay.appendChild(newDayText);
-    calender_day.appendChild(newDay);
+  const calendar_Year = document.getElementById("calendar_Year");
+  const calendar_Month = document.getElementById("calendar_Month");
+  const calendar_day = document.getElementById("calendar_day");
+  calendar_day.innerHTML = "";
+
+  const firstDay = new Date(year, month, 1).getDay();
+  const lastDay = new Date(year, month + 1, 0).getDate();
+  const prevLastDay = new Date(year, month, 0).getDate();
+  const nextFirstDay = new Date(year, month + 2, 0).getDate();
+
+  calendar_Year.textContent = year;
+  calendar_Month.textContent = month + 1;
+
+  createPrevMonthDay(calendar_day,firstDay, prevLastDay);
+  createCurrentDay(calendar_day, lastDay);
+  createNextMonthDay(calendar_day);
 }
 
-createDay();
-
-let current = new Date();
-
-function renderCalande(){
-    const year = current.getFullYear();
-    const month = current.getMonth();
-    
-    const calenderDay = document.getElementById("calendar_day");
-    
+function createCurrentDay(calendar_day, lastDay) {
+  for (let i = 1; i <= lastDay; i++) {
+    const newDay = document.createElement("div");
+    newDay.textContent = i;
+    calendar_day.appendChild(newDay);
+  }
 }
+
+function createPrevMonthDay(calendar_day, firstDay, prevLastDay) {
+  for(let i = firstDay - 1; i >= 0; i--){
+    const day = document.createElement('div');
+    day.textContent = prevLastDay - i;
+    day.classList.add("text-gray-400");
+    calendar_day.appendChild(day);
+  }
+}
+
+function createNextMonthDay(calendar_day){
+  const weekCells = calendar_day.children.length;
+  const nextDay = ( 7 - (weekCells % 7)) % 7;
+
+  for(let i = 1; i <= nextDay; i++){
+    const nextDay = document.createElement('div');
+    nextDay.textContent = i;
+    nextDay.classList.add("text-gray-400");
+    calendar_day.appendChild(nextDay);
+  }
+}
+
+function calendarControler(){
+    const prevMonth_Btn = document.getElementById("prevMonthBtn");
+    const nextMonth_Btn = document.getElementById("nextMonthBtn");
+
+    prevMonth_Btn.addEventListener("click", e => {
+        date.setMonth(date.getMonth() - 1);
+        renderCalendar();
+        console.log("이전달");
+    })
+
+    nextMonth_Btn.addEventListener("click", e => {
+        date.setMonth(date.getMonth() + 1);
+        renderCalendar();
+        console.log("다음달");
+    })
+
+}
+
+renderCalendar();
+calendarControler();
